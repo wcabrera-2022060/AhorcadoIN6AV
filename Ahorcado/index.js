1ef494820d8e9588923f658ac7f1e715fcd3d2d2
Informatica <informatica@alumno.local>
2024-01-18 17:01:16 -0600

    tercera parte del ahorcado

--- Ahorcado/index.js
+++ Ahorcado/index.js
@@ -1,8 +1,26 @@
-const palabras = [computadora, teclado, mouse];
+const palabras = ["computadora", "teclado", "mouse", "monitor", "laptop"];
 letrasUsadas = [];
-var intentos = 0;
-palabraRandom = Math.floor(Math.random() * palabras);
+var intentos = 6;
+palabraOculta = palabraRandom();
+juegoTerminado = false;
+
 
 var canvas = document.getElementById("dibujo");
 var dibujar = canvas.getContext("2d");
 
+document.getElementById("oportunidades").innerHTML = intentos;
+
+function palabraRandom(){
+    return palabras[Math.floor(Math.random() * palabras.length)];
+}
+
+document.getElementById("palabraOculta").innerHTML = palabraOculta;
+
+function registroLetras(letraNueva){
+    if(letrasUsadas.includes(letraNueva)){
+        alert("letra ya usada");
+    }else{
+        letrasUsadas.push(letraNueva);
+        document.getElementById("letrasUsadas").innerHTML = letrasUsadas;
+    }
+}
